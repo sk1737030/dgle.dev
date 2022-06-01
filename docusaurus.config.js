0f008492b643427b2a39d105d4ebb41728c2ad21
dongle <sk1737030@naver.com>
2022-06-01 15:46:24 +0900

    algolia add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,14 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'TJ88U6GSXN',
+        // Public API key: it is safe to commit it
+        apiKey: 'ed8bbc4f9cc50bea05ee0754f805bf58',
+        indexName: 'dgle.dev',
+        searchPagePath: 'search',
+      },
       navbar: {
         title: 'Dongle',
         logo: {
