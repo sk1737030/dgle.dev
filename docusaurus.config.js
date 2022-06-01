93266f2c8a7ef5c26917f766aa19007e4d9a48be
dongle <sk1737030@naver.com>
2022-06-01 16:11:01 +0900

    apikey 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
         // The application ID provided by Algolia
         appId: 'TJ88U6GSXN',
         // Public API key: it is safe to commit it
-        apiKey: 'ed8bbc4f9cc50bea05ee0754f805bf58',
+        apiKey: '46c76c9ca82c5d107ae387826d1a3606',
         indexName: 'dgle.dev',
         searchPagePath: 'search',
       },
