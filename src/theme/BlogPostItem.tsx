70ad676927271a2d78bc2dfeda3d45f0e19fb648
dongle <sk1737030@naver.com>
2022-06-01 19:06:29 +0900

    comment url 수정

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -18,7 +18,7 @@ function BlogPostItem(props) {
       const script = document.createElement("script");
 
       script.src = "https://utteranc.es/client.js";
-      script.setAttribute("repo", "sk1737030/sk1737030.github.io");
+      script.setAttribute("repo", "sk1737030/dgle.dev");
       script.setAttribute("issue-term", "pathname");
       script.setAttribute("label", "comment");
       script.setAttribute("theme", utterancesTheme);
