24e58ba380cf0eba5c25e248721b930c9513ee9c
dongho.geum <dongho.geum@myrealtrip.com>
2023-04-29 16:13:28 +0900

    feat: commnet js 추가

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlogPostPage from '@theme-original/BlogPostPage';
+import Comment from "../../components/Comment";
 
 export default function BlogPostPageWrapper(props) {
   return (
     <>
       <BlogPostPage {...props} />
+      {<Comment/>}
     </>
   );
 }
--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CodeBlock from '@theme-original/CodeBlock';
+import Comment from "../../components/Comment";
 
 export default function CodeBlockWrapper(props) {
   return (
     <>
       <CodeBlock {...props} />
+{/*      <Comment/>*/}
     </>
   );
 }
