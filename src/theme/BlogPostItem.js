1e64cbb856a0324f05a5958d3b9fb13f1d5ecace
brown <i.brown.kim2243@gmail.com>
2024-03-06 23:49:58 +0900

    Update BlogPostItem.js
    kkkk

--- src/theme/BlogPostItem.js
+++ src/theme/BlogPostItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
+import { useLocation } from "@docusaurus/router";
 import Comment from "../components/Comment";
 
+const BASIC_PATH = "/";
 export default function BlogPostItemWrapper(props) {
+  const { pathname } = useLocation();
+  const isBasicPath = BASIC_PATH === pathname;
   return (
     <>
       <BlogPostItem {...props} />
-      <br/> <br/>
-      {<Comment/>}
-      <br/>
+      <br /> <br />
+      {!isBasicPath && <Comment />}
+      <br />
     </>
   );
 }
